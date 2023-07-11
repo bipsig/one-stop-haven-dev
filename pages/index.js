d96934d52ff33ace14ff4327fe25eeb69bf3dc50
bipsig <sagnikd.ug20.ece@nitp.ac.in>
2023-07-11 18:56:38 +0530

    Created the heading and subheading section of our homepage

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import HeroBanner from "@/components/HeroBanner";
-
+import Wrapper from "@/components/Wrapper";
 
 export default function Home() {
     return (
-        <main className = "h-[2000px]">
+        <main>
             <HeroBanner />
+
+            <Wrapper>
+                <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+                    <div className="text-[24px] md:text-[30px] mb-5 font-semibold leading-tight">
+                        Sneaker Sphere: Where Sneakers Are Our Obsession
+                    </div>
+
+                    <div className="text-md md:text-xl">
+                        Sneakers crafted with luxurious leather and cushioned soles for all-day comfort. Timeless designs complement any outfit.
+                    </div>
+                </div>
+            </Wrapper>
         </main>
     );
 }
