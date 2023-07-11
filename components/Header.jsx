8a5b00539f758668291e8437403c3fc6aae47b0d
bipsig <sagnikd.ug20.ece@nitp.ac.in>
2023-07-11 16:36:22 +0530

    Designed the Header Section

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,106 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
+import Wrapper from "./Wrapper";
+import Menu from "./Menu";
+import MenuMobile from "./MenuMobile";
+
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineHeart } from "react-icons/ai";
+import { VscChromeClose } from "react-icons/vsc";
+import { CgMenuRight } from "react-icons/cg";
 
 const Header = () => {
-    return <div className="text">Header</div>;
+
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const [showCatMenu, setShowCatMenu] = useState(false);
+    const [show, setShow] = useState("translate-y-0");
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    const controlNavbar = () => {
+        if (window.scrollY > 200) {
+            if (window.scrollY > lastScrollY && !mobileMenu) {
+                setShow ("-translate-y-[80px]");
+            }
+            else {
+                setShow ("shadow-sm");
+            }
+        }
+        else {
+            setShow ("translate-y-0");
+        }
+
+        setLastScrollY (window.scrollY);
+    }
+    // When scrolled the function will be called which shall update the value of scroll amount is the variable lastScrollY. When the scroll amount is less than 200 no change is brought on. When scrolled more than 200, '-translate-y-[80px]' is added to the menu which translated the menu upwards and hence hiding it. When the scroll amount starts decreasing, that is we start scrolling upwards the header appears again.
+
+    useEffect (() => {
+        window.addEventListener ("scroll", controlNavbar);
+
+        return () => {
+            window.removeEventListener ("scroll", controlNavbar)
+        }
+    }, [lastScrollY])
+    // This function is activated whenever the page is scrolled. When scrolled controlNavbar() function is called
+
+    return (
+        <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}>
+            <Wrapper className="h-[60px] flex justify-between items-center">
+                <Link href="/" className="ml-6">
+                    <div className="flex flex-row">
+                        <img src="/logo-only.png" className="w-[40px] md:w-[60px]" alt="logo" />
+                        <img src="/logo-text.png" className="w-[40px] md:w-[60px]" alt="logo" />
+                    </div>
+                </Link>
+
+                <Menu
+                    showCatMenu={showCatMenu}
+                    setShowCatMenu={setShowCatMenu}
+                />
+
+                {mobileMenu && (
+                    <MenuMobile
+                        showCatMenu={showCatMenu}
+                        setShowCatMenu={setShowCatMenu}
+                        setMobileMenu={setMobileMenu}
+                    />
+                )}
+
+                <div className="flex items-center gap-2 text-black">
+                    <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+
+                        <AiOutlineHeart className="text-[20px] md:text-[30px]" />
+                        <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+                            21
+                        </div>
+                    </div>
+
+                    <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+
+                        <AiOutlineShoppingCart className="text-[20px] md:text-[30px]" />
+                        <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+                            5
+                        </div>
+                    </div>
+
+                    <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
+                        {mobileMenu ? (
+                            <VscChromeClose
+                                className="text-[16px]"
+                                onClick={() => setMobileMenu(false)}
+                            />
+                        ) : (
+                            <CgMenuRight
+                                className="text-[16px]"
+                                onClick={() => setMobileMenu(true)}
+                            />
+                        )}
+                    </div>
+                </div>
+            </Wrapper>
+        </header>
+    );
 };
 
 export default Header;
---
+++ components/MenuMobile.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import Link from 'next/link';
+import { IoMdArrowDropdown } from "react-icons/io";
+
+const data = [
+    { id: 1, name: "Home", url: "/" },
+    { id: 2, name: "About", url: "/about" },
+    { id: 3, name: "Categories", subMenu: true },
+    { id: 4, name: "Contact", url: "/contact" },
+];
+
+const subMenuData = [
+    { id: 1, name: "Jordan", doc_count: 11 },
+    { id: 2, name: "Sneakers", doc_count: 8 },
+    { id: 3, name: "Running Shoes", doc_count: 64 },
+    { id: 4, name: "Football Shoes", doc_count: 107 },
+];
+
+const MenuMobile = ({ showCatMenu, setShowCatMenu, setMobileMenu }) => {
+    return (
+        <div className="md:hidden font-bold absolute top-[50px] left-0 w-full bg-white border-t text-black">
+            <ul className="flex flex-col">
+                {data.map((item) => {
+                    return (
+                        <React.Fragment key={item.id}>
+                            {!!item?.subMenu ? (
+                                <li
+                                    className="cursor-pointer py-4 px-5 border-b flex flex-col relative"
+                                    onClick={() => setShowCatMenu(!showCatMenu)}
+                                >
+                                    <div className="flex justify-between items-center">
+                                        {item.name}
+                                        <IoMdArrowDropdown size={20} />
+                                    </div>
+
+                                    {showCatMenu && (
+                                        <ul className="bg-black/[0.05] -mx-5 mt-4 -mb-4">
+                                            {subMenuData.map((submenu) => {
+                                                return (
+                                                    <Link
+                                                        key={submenu.id}
+                                                        href="/"
+                                                        onClick={() => {
+                                                            showCatMenu(false);
+                                                            setMobileMenu(false);
+                                                        }}
+                                                    >
+                                                        <li className="py-4 px-8 border-t flex justify-between">
+                                                            {submenu.name}
+                                                            <span className="opacity-50 text-sm">
+                                                                {submenu.doc_count}
+                                                            </span>
+                                                        </li>
+                                                    </Link>
+                                                );
+                                            })}
+                                        </ul>
+                                    )}
+                                </li>
+                            ) : (
+                                <li className="py-4 px-5 border-b">
+                                    <Link
+                                        href={item.url}
+                                        onClick={() => setMobileMenu(false)}
+                                    >
+                                        {item.name}
+                                    </Link>
+                                </li>
+                            )}
+                        </React.Fragment>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+};
+
+export default MenuMobile;
