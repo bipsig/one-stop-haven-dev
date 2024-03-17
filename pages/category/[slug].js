462f3b51abfbfb5dd4248f110be4561da2fb47b9
bipsig <sagnikd.ug20.me@nitp.ac.in>
2024-03-17 22:28:31 +0530

    debug

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,42 +1,42 @@
-// import React from 'react';
+import React from 'react';
 
-// import Wrapper from '@/components/Wrapper';
-// import ProductCard from '@/components/ProductCard';
-// import { fetchDataFromApi } from '@/utils/api';
+import Wrapper from '@/components/Wrapper';
+import ProductCard from '@/components/ProductCard';
+import { fetchDataFromApi } from '@/utils/api';
 
-// const Category = () => {
+const Category = () => {
 
-//     return (
-//         <div className="w-full md:py-20">
-//             <Wrapper>
-//                 <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
-//                     <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-//                         Running Shoes
-//                     </div>
+    return (
+        <div className="w-full md:py-20">
+            <Wrapper>
+                <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
+                    <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+                        Running Shoes
+                    </div>
 
-//                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-//                         <ProductCard />
-//                         <ProductCard />
-//                         <ProductCard />
-//                         <ProductCard />
-//                         <ProductCard />
-//                         <ProductCard />
-//                         <ProductCard />
-//                         <ProductCard />
-//                         <ProductCard />
-//                         <ProductCard />
-//                         <ProductCard />
-//                         <ProductCard />
-//                         <ProductCard />
-//                         <ProductCard />
-//                     </div>
-//                 </div>
-//             </Wrapper>
-//         </div>
-//     )
-// }
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                    </div>
+                </div>
+            </Wrapper>
+        </div>
+    )
+}
 
-// export default Category;
+export default Category;
 
 // // export getStaticPaths = async () => {
 // //     const category = await fetchDataFromApi ("/products/categories");
